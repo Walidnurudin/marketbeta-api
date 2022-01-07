95fd8205e9067c5737a4cf3f81cd3823708a8661
Walidnurudin <walidnurudin47@gmail.com>
2022-01-08 00:39:09 +0700

    API: login

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -1,19 +1,84 @@
 const userModel = require("./userModel");
 const responseWrapper = require("../../helper/wrapper");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = {
   register: async (req, res) => {
     try {
       const { name, email, gender, password } = req.body;
 
+      if (!name || !email || !gender || !password) {
+        return responseWrapper.response(
+          res,
+          400,
+          "Please complete all field",
+          null
+        );
+      }
+
       const emailUser = await userModel.findOne({ email: email });
       if (emailUser) {
         return responseWrapper.response(res, 400, "Email already exist", null);
       }
 
-      const result = await userModel.create({ name, email, gender, password });
+      const salt = bcrypt.genSaltSync(10);
+      const passwordHash = bcrypt.hashSync(password, salt);
+
+      const result = await userModel.create({
+        name,
+        email,
+        gender,
+        password: passwordHash,
+      });
 
       return responseWrapper.response(res, 200, "Success register", result);
+    } catch (error) {
+      return responseWrapper.response(
+        res,
+        400,
+        `Bad request: ${error.message}`,
+        null
+      );
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return responseWrapper.response(
+          res,
+          400,
+          "Please complete all field",
+          null
+        );
+      }
+
+      const user = await userModel.findOne({ email: email });
+      if (!user) {
+        return responseWrapper.response(res, 400, "Email not registered", null);
+      }
+
+      const isPassword = bcrypt.compareSync(password, user.password);
+      if (!isPassword) {
+        return responseWrapper.response(res, 400, "Wrong password", null);
+      }
+
+      const newUser = { ...user.toObject() };
+      delete newUser.password;
+      delete newUser.__v;
+
+      const token = jwt.sign({ ...newUser }, process.env.SECRET_KEY, {
+        expiresIn: "24h",
+      });
+
+      return responseWrapper.response(res, 200, "Success login", {
+        ...newUser,
+        token,
+      });
     } catch (error) {
       return responseWrapper.response(
         res,

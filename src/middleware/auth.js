db26ccfda6cca58869fe409d480fd58cd86b9168
Walidnurudin <walidnurudin47@gmail.com>
2022-01-09 18:33:57 +0700

    update middleware auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 module.exports = {
   authentication: async (req, res, next) => {
     let token = req.headers.authorization;
+     if (!token) {
+      return responseWrapper.response(res, 403, "Please login first");
+    }
+
     token = token.split(" ")[1];
 
     jwt.verify(token, process.env.SECRET_KEY, (err, result) => {
